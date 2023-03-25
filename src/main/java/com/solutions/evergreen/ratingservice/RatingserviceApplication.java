007660d031ed16f5387d46a46c795910693d8ac8
Debanjan Panja 2 <debpanja0@publicisgroupe.net>
2023-03-26 00:10:09 +0530

    Rating Service

--- src/main/java/com/solutions/evergreen/ratingservice/RatingserviceApplication.java
+++ src/main/java/com/solutions/evergreen/ratingservice/RatingserviceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan({ "com.solutions.evergreen.ratingservice" })
-@EnableEurekaClient
+//@EnableEurekaClient
 @SpringBootApplication
 public class RatingserviceApplication {
 
