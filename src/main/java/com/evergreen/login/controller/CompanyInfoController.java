ebf3c2e60de021465176196ad6953c183374b10b
Debanjan Panja 2 <debpanja0@publicisgroupe.net>
2023-03-26 13:39:14 +0530

    ratings service

--- src/main/java/com/evergreen/login/controller/CompanyInfoController.java
+++ src/main/java/com/evergreen/login/controller/CompanyInfoController.java
@@ -2,6 +2,8 @@ package com.evergreen.login.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,7 +19,8 @@ public class CompanyInfoController {
 	@Autowired
 	CompanyInfoService companyInfoService;
 
-	@RequestMapping(value = "/usage")
+	@CrossOrigin
+	@PostMapping(value = "/usage")
 	public @ResponseBody CompanyInfoResponse getUsage(@RequestBody CompanyInfoRequest companyInfoRequest) {
 		return companyInfoService.getUsage(companyInfoRequest);
 	}
--- src/main/java/com/evergreen/login/service/impl/CompanyInfoServiceImpl.java
+++ src/main/java/com/evergreen/login/service/impl/CompanyInfoServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import com.evergreen.login.domain.companyinfo.CompanyInfoRequest;
 import com.evergreen.login.domain.companyinfo.CompanyInfoResponse;
+import com.evergreen.login.domain.usage.Usage;
 import com.evergreen.login.service.CompanyInfoService;
 
 @Service
@@ -11,8 +12,46 @@ public class CompanyInfoServiceImpl implements CompanyInfoService {
 
 	@Override
 	public CompanyInfoResponse getUsage(CompanyInfoRequest companyInfoRequest) {
-		// TODO Auto-generated method stub
-		return null;
+		CompanyInfoResponse companyInfoResponse = new CompanyInfoResponse();
+
+		companyInfoResponse.setSuccess(Boolean.TRUE);
+
+		Usage chemicalUsage = new Usage();
+		chemicalUsage.setWeeklyUsage("10");
+		chemicalUsage.setMonthlyUsage("100");
+		chemicalUsage.setQuarterlyUsage("1000");
+		chemicalUsage.setYearlyUsage("10000");
+		companyInfoResponse.setChemicalUsage(chemicalUsage);
+
+		Usage energyUsage = new Usage();
+		energyUsage.setWeeklyUsage("20");
+		energyUsage.setMonthlyUsage("200");
+		energyUsage.setQuarterlyUsage("2000");
+		energyUsage.setYearlyUsage("20000");
+		companyInfoResponse.setEnergyUsage(energyUsage);
+
+		Usage fabricUsage = new Usage();
+		fabricUsage.setWeeklyUsage("30");
+		fabricUsage.setMonthlyUsage("300");
+		fabricUsage.setQuarterlyUsage("3000");
+		fabricUsage.setYearlyUsage("30000");
+		companyInfoResponse.setFabricUsage(fabricUsage);
+
+		Usage rawMaterialsUsage = new Usage();
+		rawMaterialsUsage.setWeeklyUsage("40");
+		rawMaterialsUsage.setMonthlyUsage("400");
+		rawMaterialsUsage.setQuarterlyUsage("4000");
+		rawMaterialsUsage.setYearlyUsage("40000");
+		companyInfoResponse.setRawMaterialsUsage(rawMaterialsUsage);
+
+		Usage waterUsage = new Usage();
+		waterUsage.setWeeklyUsage("50");
+		waterUsage.setMonthlyUsage("500");
+		waterUsage.setQuarterlyUsage("5000");
+		waterUsage.setYearlyUsage("50000");
+		companyInfoResponse.setWaterUsage(waterUsage);
+
+		return companyInfoResponse;
 	}
 
 }
