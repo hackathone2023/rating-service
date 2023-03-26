bee8a87b143c042e5f04bd35f3c749aadf8230a2
Mohammed Khazi <mohkhazi@publicisgroupe.net>
2023-03-26 11:19:33 +0530

    added rating data

--- src/main/java/com/solutions/evergreen/ratingservice/service/CompanyInfoService.java
+++
@@ -1,9 +0,0 @@
-package com.solutions.evergreen.ratingservice.service;
-
-import com.solutions.evergreen.ratingservice.domain.companyinfo.CompanyInfoRequest;
-import com.solutions.evergreen.ratingservice.domain.companyinfo.CompanyInfoResponse;
-
-public interface CompanyInfoService {
-
-	public CompanyInfoResponse getUsage(CompanyInfoRequest companyInfoRequest);
-}
